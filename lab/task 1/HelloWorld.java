f9d1086b37b8b0f6d088473e7f47d662d529eff7
MEHEDULLAH <princeortho4@gmail.com>
2025-11-09 17:41:19 +0600

    25-62851-2

--- lab/task 1/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        int num1=2;
-        int num2=1;
-        int num3=3;
-        float avg=(num1+num2+num3)/3;
-        System.out.println("avg ="+avg);
-    }
-}
