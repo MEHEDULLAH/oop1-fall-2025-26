be13354fdfde14904993ce8c447d0277de2d9875
MEHEDULLAH <princeortho4@gmail.com>
2025-12-09 23:15:48 +0600

    25-62851-2

--- lab task/final-term_task 1/OneToOne_OneToMany.java
+++ lab task/final-term_task 1/OneToOne_OneToMany.java
@@ -30,37 +30,36 @@ class School {
 
     public void showstudents() {
         System.out.println("school " + schoolName);
-        System.out.println("students");
+        System.out.println("students" + student);
         for (int i = 0; i < count; i++) {
             System.out.println(" - " + students[i].getName());
         }
     }
 }
 class Contact {
-    private String contactnumber;
-
-    public Contact(String contactnumber) {
-        this.contactnumber = contactnumber;
-       
+    private String phone;
+    private String email;
+    public Contact(String phone, String email) {
+        this.phone = phone;
+        this.email = email;
     }
-
-    public String getnumber() {
-        return contactnumber  ;
+    public String toString() {
+        return "Phone: " + phone + ", Email: " + email;
     }
 }
 
 
 class StuDent{
     private String name;
-    private Contact contactnumber; 
-    public StuDent(String name, Contact contactnumber) {
+    private Contact contact; 
+    public StuDent(String name, Contact contact) {
         this.name = name;
-        this.contactnumber = contactnumber;
+        this.contact = contact;
     }
 
     public void showstudentInfo() {
         System.out.print("student Name: " + name);
-        System.out.println("contact: " + contactnumber);
+        System.out.println("contact: " + contact);
     }
 }
 
